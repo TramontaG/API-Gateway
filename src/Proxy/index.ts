e2d0ace978d55940fb7edf5d155b0b04c1dac911
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2022-11-22 05:57:48 +0000

    fixing proxies and adding portfolio route

--- src/Proxy/index.ts
+++ src/Proxy/index.ts
@@ -1,9 +1,9 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import type {Express} from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import type { Express } from 'express';
 import { Routes } from 'src/Router/routes';
 
 export const setupProxies = (app: Express, routes: Routes[]) => {
-    routes.forEach(({source, target, changeOrigin, pathRewrite}) => {
-        app.use('/', createProxyMiddleware({target, changeOrigin, pathRewrite}));
-    })
-}
+	routes.forEach(({ source, target, changeOrigin, pathRewrite }) => {
+		app.use(source, createProxyMiddleware({ target, changeOrigin, pathRewrite }));
+	});
+};
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import morgan from 'morgan';
 import { setupProxies } from './Proxy';
 import { ROUTES } from './Router/routes';
 
-const app = express()
+const app = express();
 const port = 8000;
 
 setupProxies(app, ROUTES);
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
