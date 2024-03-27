b3b8c0921cf7490124228419b848e86411d7446d
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2024-03-27 22:24:13 +0000

    stuff

--- src/Proxy/index.ts
+++ src/Proxy/index.ts
@@ -3,12 +3,13 @@ import type { Express } from 'express';
 import { Routes } from 'src/Router/routes';
 
 export const setupProxies = (app: Express, routes: Routes[]) => {
-	routes.forEach(({ source, target, changeOrigin }) => {
+	routes.forEach(({ source, target, changeOrigin, socketIo }) => {
 		app.use(
 			source,
 			createProxyMiddleware({
 				target,
 				changeOrigin,
+				ws: socketIo ?? false,
 				pathRewrite: {
 					[`^${source}`]: '',
 				},
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,44 @@
 import express from 'express';
-import morgan from 'morgan';
+import https from 'https';
 import { setupProxies } from './Proxy';
 import { ROUTES } from './Router/routes';
+import fs from 'fs';
 
 const app = express();
+const httpsApp = express();
 const port = 8000;
 
-setupProxies(app, ROUTES);
+[app, httpsApp].forEach(instance => {
+	instance.use('/', (req, res, next) => {
+		console.log(`[${new Date().toISOString()}] ${req.url}`);
 
-app.use(morgan('combined'));
+		next();
+	});
+});
+
+httpsApp.get('/', (req, res) => {
+	res.send('ok');
+});
+
+setupProxies(app, ROUTES);
+setupProxies(httpsApp, ROUTES);
+
+app.use((req, res, next) => {
+	console.log(req.url);
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
+
+https
+	.createServer(
+		{
+			key: fs.readFileSync('./Certs/server.key'),
+			cert: fs.readFileSync('./Certs/server.cert'),
+		},
+		httpsApp
+	)
+	.listen(4433, () => {
+		console.log(`Example app listening at https://localhost:${4433}`);
+	});
