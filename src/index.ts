92dde3a2ab15001ce338aff54c119286ddbdb3fc
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2026-03-16 01:19:42 -0300

    removing https bloatware

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,3 @@ const port = 8000;
 setupProxies(app, ROUTES);
 
 app.listen(port);
-
-https
-	.createServer(
-		{
-			key: fs.readFileSync('./Certs/server.key'),
-			cert: fs.readFileSync('./Certs/server.cert'),
-		},
-		httpsApp
-	)
-	.listen(4433, () => {
-		console.log(`Example app listening at https://localhost:${4433}`);
-	});
